90dcf2a8ed48d8fbfefa1495cbe4c3e756299858
sankalp125 <sankalp.tiwari125@gmail.com>
2023-04-08 00:34:12 +0530

    some functionality added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
     Button bequal;
     Button bdiv;
     Button bclear;
+    int sum=0;
+    int sub=0;
+    int mul=0;
+    int div=0;
+    int flag=0;
+    int x=0;
+    int y=0;
+    int justclicked=0;
+    int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,21 +56,327 @@ public class MainActivity extends AppCompatActivity {
        bdiv=findViewById(R.id.bdiv);
        bequal=findViewById(R.id.bequal);
        bclear=findViewById(R.id.bclear);
-       int sum=0;
-       int sub=0;
-       int mul=0;
-       int div=0;
-       int flag=0;
-       int x=0;
-       int y=0;
+
        try {
            b1.setOnClickListener(new View.OnClickListener() {
                @Override
                public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10+1;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else{
+                       y=y*10+1;
+                       if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
 
+                   }
 
                }
            });
+           b2.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10+2;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else{
+                       y=y*10+2;
+                       if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
+
+                   }
+               }
+           });
+           b3.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10+3;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else {
+                       y=y*10+3;
+                       if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
+                   }
+               }
+           });
+           b4.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10+4;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else{
+                       y=y*10+4;
+                       if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
+                   }
+               }
+           });
+           b5.setOnClickListener(new View.OnClickListener() {
+
+               @Override
+               public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10+5;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else{
+                       y=y*10+5; if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
+                   }
+               }
+           });
+           b6.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10+6;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else{
+                       y=y*10+6;
+                       if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
+                   }
+               }
+           });
+           b7.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10+7;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else {
+                       y=y*10+7;
+                       if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
+                   }
+               }
+           });
+           b8.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10+8;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else{
+                       y=y*10+8;
+                       if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
+                   }
+               }
+           });
+           b9.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10+9;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else{
+                       y=y*10+9;
+                       if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
+                   }
+               }
+           });
+           b0.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   if(justclicked==1){
+                       justclicked=0;
+                   }
+                   if(flag==0){
+                       x=x*10;
+                       t1.setText(String.valueOf(x));
+                   }
+                   else{
+                       y=y*10;
+                       if(sum==1){
+                           t1.setText(String.valueOf(x)+"+"+String.valueOf(y));
+                       } else if (sub==1) {
+                           t1.setText(String.valueOf(x)+"-"+String.valueOf(y));
+                       } else if (mul==1) {
+                           t1.setText(String.valueOf(x)+"x"+String.valueOf(y));
+                       }
+                       else{
+                           t1.setText(String.valueOf(x)+"%"+String.valueOf(y));
+                       }
+                   }
+               }
+           });
+           bsum.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   flag=1;
+                   sum=1;
+                   justclicked=1;
+                   t1.setText(String.valueOf(x)+"+");
+               }
+           });
+           bsub.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   flag=1;
+                   sub=1;
+                   justclicked=1;
+                   t1.setText(String.valueOf(x)+"-");
+               }
+           });
+           bmult.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   flag=1;
+                   mul=1;
+                   justclicked=1;
+                   t1.setText(String.valueOf(x)+"x");
+               }
+           });
+           bdiv.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   flag=1;
+                   div=1;
+                   justclicked=1;
+                   t1.setText(String.valueOf(x)+"%");
+               }
+           });
+           bequal.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                 if(flag==1){
+                     if(sum==1){
+                         result=x+y;
+                     } else if (sub==1) {
+                         result=x-y;
+                     }
+                     else if(mul==1){
+                         result=x*y;
+                     }
+                     else {
+                         result=x/y;
+                     }
+                     t1.setText(String.valueOf(result));
+                     x=result;
+                     flag=0;
+                     sum=0;
+                     sub=0;
+                     mul=0;
+                     div=0;
+                 }
+                 else{
+                     t1.setText(String.valueOf(x));
+                 }
+               }
+           });
+
+
        }catch (NullPointerException ex){
            ex.printStackTrace();
        }
